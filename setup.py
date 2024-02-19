bf547e24252feef20cb7f60c69e06c7ab45de937
pmuser1234 <131592953+pmuser1234@users.noreply.github.com>
2024-02-19 13:43:26 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
+
 
 package_name = 'igvcbot'
 
@@ -10,6 +13,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share',package_name,'launch'),glob('launch/*')),
+        (os.path.join('share',package_name,'urdf'),glob('urdf/*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
