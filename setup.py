a81c692c081f83174d114879bfe9832e3ecaab97
pmuser1234 <131592953+pmuser1234@users.noreply.github.com>
2024-03-17 11:18:54 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,9 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share',package_name,'launch'),glob('launch/*')),
-        (os.path.join('share',package_name,'urdf'),glob('urdf/*'))
+        (os.path.join('share',package_name,'urdf'),glob('urdf/*')),
+        (os.path.join('share',package_name,'config'),glob('config/*')),
+        
     ],
     install_requires=['setuptools'],
     zip_safe=True,
